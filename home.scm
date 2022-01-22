197213af5ab9dec936e18b0a99b11f1f8e8371dc
Karl Hallsby <karl@hallsby.com>
2022-01-21 22:23:22 -0600

    Create a home.scm, which provides 2 emacs servers
    The emacs servers are currently parameterized according to the
    `emacs-server` function. All that the user must do is call the
    function. If the `server-name` is empty, then a default of `server` is
    used.

---
+++ home.scm
@@ -0,0 +1,64 @@
+(use-modules (gnu home)
+             (gnu home services)
+             (gnu home services shells)
+             (gnu home services shepherd)
+             (gnu services)
+             (gnu packages admin)
+             (gnu packages password-utils)
+             (gnu packages emacs)
+             (guix gexp))
+
+(define (emacs-server server-name)
+  (define (server-name->symbol server-name)
+    (let ((emacs-server-symbol 'emacs-server))
+      (if (string-null? server-name)
+          emacs-server-symbol
+          (symbol-append emacs-server-symbol (string->symbol "-")
+                         (string->symbol server-name)))))
+  (define (server-name->server-socket-name server-name)
+    (if (string-null? server-name) "server" server-name))
+
+  (shepherd-service
+   (provision (list
+               (server-name->symbol server-name)));; (server-name-suffix server-name))))
+   (requirement '())
+   (respawn? #t)
+   (start #~(make-forkexec-constructor
+             (list #$(file-append emacs "/bin/emacs")
+                   #$(format #f "--fg-daemon=~a"
+                             (server-name->server-socket-name server-name)))
+             #:log-file (string-append
+				                 (or (getenv "XDG_LOG_HOME")
+				                     (format #f "~a/.local/var/log"
+					                           (getenv "HOME")))
+				                 #$(format #f "/emacs-~a.log"
+                                   (server-name->server-socket-name server-name)))))
+   (stop ;; #~(make-kill-destructor)
+         #~(make-system-destructor
+            #$(file-append emacs "/bin/emacsclient" " "
+                           (format #f "--socket-name=~a"
+                                   (server-name->server-socket-name server-name))
+                                 " " "--eval (kill-emacs)"))
+         )
+   (documentation (string-append "Emacs server"
+                                 (if (string-null? server-name)
+                                     ""
+                                     (string-append " for " server-name))))))
+
+(home-environment
+ (packages
+  (list password-store ;; pass
+        ))
+ (services
+  (list
+   (service home-bash-service-type
+            (home-bash-configuration
+             (guix-defaults? #t)))
+   (service home-shepherd-service-type
+            (home-shepherd-configuration
+   ;;           (auto-start? #t)
+             (services
+              (list
+               (emacs-server "")
+               (emacs-server "debug")))))
+   )))
