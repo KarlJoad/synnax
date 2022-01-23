02f0c039f75089de652145758d032f3d234381d7
Karl Hallsby <karl@hallsby.com>
2022-01-22 19:58:50 -0600

    Desktop: Build kernel with Sound Blaster kernel modules too
    Defined a variadic function that takes in as many alists as we pass to
    it, flattens them to a single alist which is then prepended to the
    list of default linux-libre kernel config flags.

--- desktop.scm
+++ desktop.scm
@@ -11,7 +11,6 @@
  virtualization)
 
 (define %ipmi-linux-options
-  (append
    `(("CONFIG_ACPI_IPMI" . #t)
      ("CONFIG_IPMI_HANDLER" . #t)
      ("CONFIG_IPMI_DMI_DECODE" . #t)
@@ -21,21 +20,32 @@
      ("CONFIG_IPMI_SI" . #t)
      ("CONFIG_IPMI_SSIF" . #t)
      ("CONFIG_IPMI_WATCHDOG" . #t)
-     ("CONFIG_IPMI_POWEROFF" . #t))
+     ("CONFIG_IPMI_POWEROFF" . #t)))
+
+(define %sound-card-options
+  `(("CONFIG_SND_EMU10K1" . #t)))
+
+(define* (add-linux-libre-config-options #:rest options)
+  "Prepend provided list of OPTIONS, which itself is a list of pairs."
+  (append
+   (apply append options)
    (@@ (gnu packages linux) %default-extra-linux-options)))
 
-(define-public linux-libre/all-ipmi
+(define-public linux-libre/desktop
   ((@@ (gnu packages linux) make-linux-libre*)
    linux-libre-version
    linux-libre-gnu-revision
    linux-libre-source
    '("x86_64-linux")
    #:configuration-file (@@ (gnu packages linux) kernel-config)
-   #:extra-options %ipmi-linux-options))
+   ;; extra-options must take ALL config flags to be passed to the kernel!
+   #:extra-options (add-linux-libre-config-options
+                    %ipmi-linux-options
+                    %sound-card-options)))
 
 (operating-system
  (locale "en_US.utf8")
- (kernel linux-libre/all-ipmi)
+ (kernel linux-libre/desktop)
 
  (timezone "America/Chicago")
  (keyboard-layout (keyboard-layout "us"))
