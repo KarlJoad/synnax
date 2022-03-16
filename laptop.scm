92db34d37e9c0bc5892c7934300daac3b620d460
Karl Hallsby <karl@hallsby.com>
2022-03-15 19:15:51 -0500

    Use nonguix full Linux kernel for laptop
    Needed for WiFi

--- laptop.scm
+++ laptop.scm
@@ -1,7 +1,10 @@
 (use-modules (gnu)
              (gnu packages linux)
              (guix download)
-             (guix packages))
+             (guix packages)
+             (nongnu packages linux)
+             (nongnu system linux-initrd))
+
 (use-service-modules
  cups
  desktop
@@ -10,43 +13,11 @@
  xorg
  virtualization)
 
-(define %iwlwifi-linux-options
-  `(("CONFIG_IWLWIFI" . #t)
-    ("CONFIG_IWLWIFI_LEDS" . #t)
-    ("CONFIG_IWLDVM" . #t)
-    ("CONFIG_IWLVMV" . #t)))
-
-(define %video-card-options
-  `(("CONFIG_DRM_I915" . #t)
-    ("CONFIG_DRM_I915_CAPTURE_ERROR" . #t)
-    ("CONFIG_DRM_I915_COMPRESS_ERROR" . #t)
-    ("CONFIG_DRM_I915_USERPTR" . #t)
-    ("CONFIG_DRM_I915_GVT" . #t)
-    ("CONFIG_DRM_I915_GVT_KVMGT" . #t)
-    ("CONFIG_DRM_I915_PXP" . #t)
-    ))
-
-(define* (add-linux-libre-config-options #:rest options)
-  "Prepend provided list of OPTIONS, which itself is a list of pairs."
-  (append
-   (apply append options)
-   (@@ (gnu packages linux) %default-extra-linux-options)))
-
-(define-public linux-libre/desktop
-  ((@@ (gnu packages linux) make-linux-libre*)
-   linux-libre-version
-   linux-libre-gnu-revision
-   linux-libre-source
-   '("x86_64-linux")
-   #:configuration-file (@@ (gnu packages linux) kernel-config)
-   ;; extra-options must take ALL config flags to be passed to the kernel!
-   #:extra-options (add-linux-libre-config-options
-                    %iwlwifi-linux-options
-                    %video-card-options)))
-
 (operating-system
  (locale "en_US.utf8")
- (kernel linux-libre/desktop)
+ (kernel linux)
+ (initrd microcode-initrd)
+ (firmware (list linux-firmware))
 
  (timezone "America/Chicago")
  (keyboard-layout (keyboard-layout "us"))
