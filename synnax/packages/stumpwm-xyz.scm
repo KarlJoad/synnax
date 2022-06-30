928f8cee417a60a5eccf8596a12337634d1b0249
Karl Hallsby <karl@hallsby.com>
2022-06-29 23:20:18 -0500

    stumpwm-contrib: move definition of source information out of packages
    This way, each package can have its information "inherited" from a
    single symbol that defines the necessary information.

--- synnax/packages/stumpwm-xyz.scm
+++ synnax/packages/stumpwm-xyz.scm
@@ -12,56 +12,57 @@
 
 ;; Packages install to their directories.
 
+(define stumpwm-contrib-git-branch "master")
+(define stumpwm-contrib-git-commit "d0c05077eca5257d33083de949c10bca4aac4242")
+(define stumpwm-contrib-revision "1")
+(define stumpwm-contrib-version
+  (git-version stumpwm-contrib-git-branch stumpwm-contrib-revision
+               stumpwm-contrib-git-commit))
+
 (define-public stumpwm-contrib-battery-portable
-  (let ((git-branch "master")
-        (commit "d0c05077eca5257d33083de949c10bca4aac4242")
-        (revision "1"))
-    (package
-     (name "stumpwm-contrib-battery-portable")
-     (version (git-version git-branch revision commit))
-     (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://github.com/stumpwm/stumpwm-contrib.git")
-                    (commit git-branch)))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32
-                "0zxhqh9wjfk7zas67kmwfx0a47y8rxmh8f1a5rcs300bv1083lkb"))))
-     (build-system copy-build-system)
-     (arguments
-      '(#:install-plan
-        ;; Install the contents of battery-portable to the root of #$out
-        '(("modeline/battery-portable/" ""))))
-     (home-page "https://github.com/stumpwm/stumpwm-contrib/tree/master/modeline/battery-portable")
-     (synopsis "Display battery information in StumpWM's modeline")
-     (description "StumpWM-contributed module to portably be able to display the
+  (package
+   (name "stumpwm-contrib-battery-portable")
+   (version stumpwm-contrib-version)
+   (source (origin
+            (method git-fetch)
+            (uri (git-reference
+                  (url "https://github.com/stumpwm/stumpwm-contrib.git")
+                  (commit stumpwm-contrib-git-branch)))
+            (file-name (git-file-name name version))
+            (sha256
+             (base32
+              "0zxhqh9wjfk7zas67kmwfx0a47y8rxmh8f1a5rcs300bv1083lkb"))))
+   (build-system copy-build-system)
+   (arguments
+    '(#:install-plan
+      ;; Install the contents of battery-portable to the root of #$out
+      '(("modeline/battery-portable/" ""))))
+   (home-page "https://github.com/stumpwm/stumpwm-contrib/tree/master/modeline/battery-portable")
+   (synopsis "Display battery information in StumpWM's modeline")
+   (description "StumpWM-contributed module to portably be able to display the
 device's battery levels in StumpWM's modeline.")
-     (license license:gpl3))))
+   (license license:gpl3)))
 
 (define-public stumpwm-contrib-cpu
-  (let ((git-branch "master")
-        (commit "d0c05077eca5257d33083de949c10bca4aac4242")
-        (revision "1"))
-    (package
-     (name "stumpwm-contrib-cpu")
-     (version (git-version git-branch revision commit))
-     (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://github.com/stumpwm/stumpwm-contrib.git")
-                    (commit git-branch)))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32
-                "0zxhqh9wjfk7zas67kmwfx0a47y8rxmh8f1a5rcs300bv1083lkb"))))
-     (build-system copy-build-system)
-     (arguments
-      '(#:install-plan
-        '(("modeline/cpu/" ""))))
-     (home-page "https://github.com/stumpwm/stumpwm-contrib/tree/master/modeline/cpu")
-     (synopsis "Display CPU information in StumpWM's modeline")
-     (description "StumpWM-contributed module to display the device's CPU usage
+  (package
+   (name "stumpwm-contrib-cpu")
+   (version stumpwm-contrib-version)
+   (source (origin
+            (method git-fetch)
+            (uri (git-reference
+                  (url "https://github.com/stumpwm/stumpwm-contrib.git")
+                  (commit stumpwm-contrib-git-branch)))
+            (file-name (git-file-name name version))
+            (sha256
+             (base32
+              "0zxhqh9wjfk7zas67kmwfx0a47y8rxmh8f1a5rcs300bv1083lkb"))))
+   (build-system copy-build-system)
+   (arguments
+    '(#:install-plan
+      '(("modeline/cpu/" ""))))
+   (home-page "https://github.com/stumpwm/stumpwm-contrib/tree/master/modeline/cpu")
+   (synopsis "Display CPU information in StumpWM's modeline")
+   (description "StumpWM-contributed module to display the device's CPU usage
 in StumpWM's modeline.")
-     (license license:gpl3))))
+   (license license:gpl3)))
 
