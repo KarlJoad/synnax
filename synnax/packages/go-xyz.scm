b4e05057378bff3af50e13c57c642dd35e80031c
Karl Hallsby <karl@hallsby.com>
2022-07-10 14:10:52 -0500

    Start module for Go modules that I will need for wally-cli

---
+++ synnax/packages/go-xyz.scm
@@ -0,0 +1,8 @@
+(define-module (synnax packages go-xyz)
+  #:use-module (guix gexp)
+  #:use-module (guix packages)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system go)
+  #:use-module (gnu packages golang)
+  #:use-module ((guix licenses) #:prefix license:))
+
