3005d200cdc0ccb3a6468c692058d5758fb3d59b
Karl Hallsby <karl@hallsby.com>
2022-07-10 14:14:57 -0500

    go-xyz: init go-github-com-google-gousb at v2.1.0

--- synnax/packages/go-xyz.scm
+++ synnax/packages/go-xyz.scm
@@ -4,6 +4,8 @@
   #:use-module (guix git-download)
   #:use-module (guix build-system go)
   #:use-module (gnu packages golang)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages libusb)
   #:use-module ((guix licenses) #:prefix license:))
 
 (define-public go-github-com-carrlos0-spin
@@ -25,4 +27,30 @@
    (synopsis "A very simple spinner for cli golang apps")
    (description "A very simple spinner for cli golang apps")
    (license license:expat)))
+
+(define-public go-github-com-google-gousb
+  (package
+   (name "go-github-com-google-gousb")
+   (version "2.1.0")
+   (source (origin
+            (method git-fetch)
+            (uri (git-reference
+                  (url "https://github.com/google/gousb")
+                  (commit (string-append "v" version))))
+            (file-name (git-file-name name version))
+            (sha256
+             (base32
+              "1aki6hk009sicrf7gxy5nkjmj4j7lsy0by4kjgd9bwq8ragfyv5x"))))
+   (native-inputs (list pkg-config libusb))
+   (build-system go-build-system)
+   (arguments
+    `(#:import-path "github.com/google/gousb"
+      #:phases (modify-phases %standard-phases
+                 ;; Delete the check phase because libusbContext and
+                 ;; libusbDevHandle cannot be allocated in Go.
+                 (delete 'check))))
+   (home-page "https://github.com/google/gousb")
+   (synopsis "Go-like bindings around the libusb library")
+   (description "Go-like bindings around the libusb library")
+   (license license:asl2.0)))
 
