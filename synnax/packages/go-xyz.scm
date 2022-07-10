cc482ed02e520837bca31cd06aab8e770318b37b
Karl Hallsby <karl@hallsby.com>
2022-07-10 14:11:30 -0500

    go-xyz: init go-github-carrlos0-spin at v1.1.0
    This particular package has been REPLACED by wally-cli upstream for
    several months. However, it was done after the last stable wally-cli
    release, so I will continue to use this slightly nebulously licensed
    spin for now.

--- synnax/packages/go-xyz.scm
+++ synnax/packages/go-xyz.scm
@@ -6,3 +6,23 @@
   #:use-module (gnu packages golang)
   #:use-module ((guix licenses) #:prefix license:))
 
+(define-public go-github-com-carrlos0-spin
+  (package
+   (name "go-github-com-carrlos0-spin")
+   (version "1.1.0")
+   (source (origin
+            (method git-fetch)
+            (uri (git-reference
+                  (url "https://github.com/caarlos0-graveyard/spin")
+                  (commit (string-append "v" version))))
+            (file-name (git-file-name name version))
+            (sha256
+             (base32
+              "1pnijds4145j8nsxvq8203r2sg2pbk7x8prkdg2ilghhrzqj6vyc"))))
+   (build-system go-build-system)
+   (arguments '(#:import-path "github.com/caarlos0/spin"))
+   (home-page "https://github.com/caarlos0-graveyard/spin")
+   (synopsis "A very simple spinner for cli golang apps")
+   (description "A very simple spinner for cli golang apps")
+   (license license:expat)))
+
