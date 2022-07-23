42b238bfeadb1345b28ab079d439bf7c0497498a
Karl Hallsby <karl@hallsby.com>
2022-07-23 15:45:25 -0500

    stumpwm: add stumpwm without documentation
    This is an example for how I can inherit a package and mess around
    with JUST the phases in the build process.
    
    Inheritance is as strong as it gets: you start off with a record that
    is identical to the inherited one, then only the fields you
    specify (below it) are replaced. The replacement is complete, though:
    if you write `(inherit foo) (arguments '(blah))`, no trace of foo's
    arguments will remain. If you want to mix at the level of phases, use
    `substitute-keyword-arguments` and `modify-phases` on the inherited
    ones.
    You can do this even without inheriting.

--- synnax/packages/wm.scm
+++ synnax/packages/wm.scm
@@ -1,9 +1,19 @@
 (define-module (synnax packages wally)
   #:use-module (guix gexp)
+  #:use-module (guix utils)
   #:use-module (guix packages)
   #:use-module (gnu packages wm)
   #:use-module (gnu packages pkg-config)
   #:use-module ((guix licenses) #:prefix license:))
+
+(define-public stumpwm-no-docs
+  (package
+    (inherit stumpwm)
+    (arguments
+     (substitute-keyword-arguments (package-arguments stumpwm)
+       ((#:phases old-phases)
+        #~(modify-phases #$old-phases
+            (delete 'install-manual)))))))
 
 (define-public stumpwm-with-contrib
   (package
