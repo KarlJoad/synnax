e8bbf44e188e9dd1b7541f3c989d7a10dbf11ff9
Karl Hallsby <karl@hallsby.com>
2022-07-24 12:23:33 -0500

    Remove unneeded test stumpwm versions

--- synnax/packages/wm.scm
+++ synnax/packages/wm.scm
@@ -7,36 +7,6 @@
   #:use-module (gnu packages pkg-config)
   #:use-module ((guix licenses) #:prefix license:))
 
-(define-public stumpwm-no-docs
-  (package
-    (inherit stumpwm)
-    (arguments
-     (substitute-keyword-arguments (package-arguments stumpwm)
-       ((#:phases old-phases)
-        #~(modify-phases #$old-phases
-            (delete 'install-manual)))))))
-
-(define-public stumpwm-with-patch
-  (package
-    (inherit stumpwm)
-    (source (origin (inherit (package-source stumpwm))
-                    (patches
-                     (append (list (local-file "patches/stumpwm-module-dir-easy-regexp-replace.patch"))
-                             ;; (search-patches "stumpwm-module-dir-easy-regexp-replace.patch")
-                             (origin-patches (package-source stumpwm))))))
-    (arguments
-     (substitute-keyword-arguments (package-arguments stumpwm)
-       ((#:phases old-phases)
-        #~(modify-phases #$old-phases
-            (add-before 'copy-source 'module-dir-point-to-store
-              (lambda* (#:key source outputs #:allow-other-keys)
-                (let ((out (assoc-ref outputs "out")))
-                  (format #t "Modifying module.lisp to point to ~s~%" out)
-                  (substitute* "module.lisp"
-                    (("REPLACE-ME")
-                     (string-append out "/modules"))))))
-            (delete 'install-manual)))))))
-
 (define-public stumpwm-with-patch-with-contrib
   (package
     (inherit stumpwm)
