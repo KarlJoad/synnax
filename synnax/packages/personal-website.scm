80e2cf56f106612689461b344f70476e7aef393f
Karl Hallsby <karl@hallsby.com>
2022-08-08 19:58:57 -0500

    website: use revision to make version more unique

--- synnax/packages/personal-website.scm
+++ synnax/packages/personal-website.scm
@@ -8,37 +8,38 @@
   #:use-module (synnax packages resume))
 
 (define-public personal-website
-  (package
-   (name "karl-personal-website")
-   (version "git")
-   (source (origin
-            (method git-fetch)
-            (uri (git-reference
-                  (url "git://git.karl.hallsby.com/haunt-website.git")
-                  (commit "master")))
-            (file-name (git-file-name name version))
-            (sha256
-             (base32
-              "1kzy20p425xlxcy91kdpkmqfck5bd6clj1x78qj1n7b1z1mhp5k4"))))
-   (build-system gnu-build-system)
-   (arguments
-    (list
-     #:phases
-     #~(modify-phases %standard-phases
-                      (delete 'configure)
-                      (delete 'check)
-                      (replace 'install
-                               (lambda _ (copy-recursively "site" #$output)
-                                       (mkdir-p (string-append #$output "/assets/pdf/resume"))
-                                       (symlink #$resume (string-append #$output "/assets/pdf/resume/Hallsby_Karl.pdf")))))))
-   (native-inputs
-    `(("guile" ,guile-3.0)
-      ("haunt" ,haunt)
-      ("guile-reader" ,guile-reader)
-      ("guile-commonmark" ,guile-commonmark)
-      ("guile-syntax-highlight", guile-syntax-highlight)))
-   (home-page "https://karl.hallsby.com")
-   (synopsis "Personal website built using Haunt static site generator")
-   (description "Karl Hallsby's personal website built using the Haunt static site
+  (let ((revision "0"))
+    (package
+     (name "karl-personal-website")
+     (version (string-append "git" "-" revision))
+     (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "git://git.karl.hallsby.com/haunt-website.git")
+                    (commit "master")))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "1kzy20p425xlxcy91kdpkmqfck5bd6clj1x78qj1n7b1z1mhp5k4"))))
+     (build-system gnu-build-system)
+     (arguments
+      (list
+       #:phases
+       #~(modify-phases %standard-phases
+                        (delete 'configure)
+                        (delete 'check)
+                        (replace 'install
+                                 (lambda _ (copy-recursively "site" #$output)
+                                         (mkdir-p (string-append #$output "/assets/pdf/resume"))
+                                         (symlink #$resume (string-append #$output "/assets/pdf/resume/Hallsby_Karl.pdf")))))))
+     (native-inputs
+      `(("guile" ,guile-3.0)
+        ("haunt" ,haunt)
+        ("guile-reader" ,guile-reader)
+        ("guile-commonmark" ,guile-commonmark)
+        ("guile-syntax-highlight", guile-syntax-highlight)))
+     (home-page "https://karl.hallsby.com")
+     (synopsis "Personal website built using Haunt static site generator")
+     (description "Karl Hallsby's personal website built using the Haunt static site
 generator.")
-   (license #f)))
+     (license #f))))
