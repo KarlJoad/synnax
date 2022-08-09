82f10726f8674e915db020e80e894e689e585839
Karl Hallsby <karl@hallsby.com>
2022-08-08 19:59:14 -0500

    resume: use revision to make version more unique

--- synnax/packages/resume.scm
+++ synnax/packages/resume.scm
@@ -6,30 +6,31 @@
   #:use-module (gnu packages tex))
 
 (define-public resume
-  (package
-   (name "karl-resume")
-   (version "git")
-   (source (origin
-            (method git-fetch)
-            (uri (git-reference
-                  (url "git://git.karl.hallsby.com/resume.git")
-                  (commit "master")))
-            (file-name (git-file-name name version))
-            (sha256
-             (base32
-              "0bqgl63gi27vzhsrb8mc8xdhbrjaccgyjixjii0p3n9lw56slag7"))))
-   (build-system gnu-build-system)
-   (native-inputs
-    (list texlive))
-   (arguments
-    (list
-     #:phases
-     #~(modify-phases %standard-phases
-                      (delete 'configure)
-                      (delete 'check)
-                      (replace 'install
-                               (lambda _ (copy-file "Hallsby_Karl.pdf" #$output))))))
-   (home-page "https://karl.hallsby.com")
-   (synopsis "Personal Resume/CV built using LaTeX")
-   (description "Karl Hallsby's personal resume/curriculum vitae built using LaTeX.")
-   (license #f)))
+  (let ((revision "0"))
+    (package
+     (name "karl-resume")
+     (version (string-append "git" "-" revision))
+     (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "git://git.karl.hallsby.com/resume.git")
+                    (commit "master")))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "0bqgl63gi27vzhsrb8mc8xdhbrjaccgyjixjii0p3n9lw56slag7"))))
+     (build-system gnu-build-system)
+     (native-inputs
+      (list texlive))
+     (arguments
+      (list
+       #:phases
+       #~(modify-phases %standard-phases
+                        (delete 'configure)
+                        (delete 'check)
+                        (replace 'install
+                                 (lambda _ (copy-file "Hallsby_Karl.pdf" #$output))))))
+     (home-page "https://karl.hallsby.com")
+     (synopsis "Personal Resume/CV built using LaTeX")
+     (description "Karl Hallsby's personal resume/curriculum vitae built using LaTeX.")
+     (license #f))))
