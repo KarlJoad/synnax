1e244103a439a2a85f6fdec8c12e44b1b8261685
Karl Hallsby <karl@hallsby.com>
2022-08-24 09:33:14 -0500

    sysadmin: cuirass: Add package to install cuirass email config
    sysadmin: cuirass-mailer: remove unnecessary srfi-13 import
    
    This does nothing but make it cleaner to read.

--- synnax/sysadmin/cuirass-mailer.scm
+++ synnax/sysadmin/cuirass-mailer.scm
@@ -27,4 +27,27 @@
 \tuser karl@hallsby.com
 \tpassword something"))
 
-(define-public cuirass-mailer-script)
+(define-public cuirass-mailer-config
+  (let ((version "git")
+        (revision "0"))
+    (package
+     (name "cuirass-mailer-config")
+     (version (string-append version "-" revision))
+     (source #f)
+     (build-system trivial-build-system)
+     (native-inputs `(("config" ,mailer-config)))
+     (arguments
+      `(#:modules ((guix build utils))
+        #:builder
+        (begin
+          (use-modules (guix build utils))
+          (let* ((mailer-config (assoc-ref %build-inputs "config"))
+                 (install-target (assoc-ref %outputs "out")))
+            (symlink mailer-config install-target)))))
+     (home-page "https://github.com/KarlJoad/synnax")
+     (synopsis "Configuration file for Cuirass emailer")
+     (description "Configuration file for Cuirass emailer.
+Connects to my personal email account and sends emails when a Cuirass evaluation
+starts failing or builds start failing.")
+     (license #f))))
+
