5eebe1c6be5e5675a277a75f4dd1a3664bbb184d
Karl Hallsby <karl@hallsby.com>
2022-12-27 19:46:43 -0600

    systems: laptop: make laptop a public module

--- modules/synnax/systems/laptop.scm
+++ modules/synnax/systems/laptop.scm
@@ -17,69 +17,70 @@
  xorg
  virtualization docker)
 
-(operating-system
- (locale "en_US.utf8")
- (kernel linux)
- (initrd microcode-initrd)
- (firmware (list linux-firmware))
+(define-public laptop
+  (operating-system
+   (locale "en_US.utf8")
+   (kernel linux)
+   (initrd microcode-initrd)
+   (firmware (list linux-firmware))
 
- (timezone "America/Chicago")
- (keyboard-layout (keyboard-layout "us"))
- (host-name "Guix-Laptop")
- (users (cons* (user-account
-                (name "karljoad")
-                (comment "Karl Hallsby")
-                (group "users")
-                (home-directory "/home/karljoad")
-                (supplementary-groups
-                 `("wheel" "netdev" "audio" "video"
-                   "lp" "input" "tty"
-                   "kvm" ;; ,(user-group (name "libvirt-users") (system? #t))
-                   "dialout")))
-               %base-user-accounts))
- (packages
-  (append
-   (list )
-   %system-packages
-   %base-packages))
+   (timezone "America/Chicago")
+   (keyboard-layout (keyboard-layout "us"))
+   (host-name "Guix-Laptop")
+   (users (cons* (user-account
+                  (name "karljoad")
+                  (comment "Karl Hallsby")
+                  (group "users")
+                  (home-directory "/home/karljoad")
+                  (supplementary-groups
+                   `("wheel" "netdev" "audio" "video"
+                     "lp" "input" "tty"
+                     "kvm" ;; ,(user-group (name "libvirt-users") (system? #t))
+                     "dialout")))
+                 %base-user-accounts))
+   (packages
+    (append
+     (list )
+     %system-packages
+     %base-packages))
 
- (services
-  (append
-   (list (service xfce-desktop-service-type)
-         (service openssh-service-type)
-         (service cups-service-type)
-         (set-xorg-configuration
-          (xorg-configuration
-           (keyboard-layout keyboard-layout)))
-         (service syncthing-service-type
-                  (syncthing-configuration
-                   (user "karljoad"))) ;; TODO: Refactor `user' field to use variable.
-         (service libvirt-service-type
-                  (libvirt-configuration
-                   (unix-sock-group "libvirt-users")))
-         (service virtlog-service-type)
-         (service docker-service-type))
-   %desktop-services))
+   (services
+    (append
+     (list (service xfce-desktop-service-type)
+           (service openssh-service-type)
+           (service cups-service-type)
+           (set-xorg-configuration
+            (xorg-configuration
+             (keyboard-layout keyboard-layout)))
+           (service syncthing-service-type
+                    (syncthing-configuration
+                     (user "karljoad"))) ;; TODO: Refactor `user' field to use variable.
+           (service libvirt-service-type
+                    (libvirt-configuration
+                     (unix-sock-group "libvirt-users")))
+           (service virtlog-service-type)
+           (service docker-service-type))
+     %desktop-services))
 
- (bootloader
-  (bootloader-configuration
-   (bootloader grub-efi-bootloader)
-   (targets (list "/boot/efi"))
-   (keyboard-layout keyboard-layout)))
- (mapped-devices
-  (list (mapped-device
-         (source
-          (uuid "34627d93-4e91-42e8-8eb8-b64c8fa4729f"))
-         (target "cryptroot")
-         (type luks-device-mapping))))
- (file-systems
-  (cons* (file-system
-          (mount-point "/boot/efi")
-          (device (uuid "C899-DB8A" 'fat32))
-          (type "vfat"))
-         (file-system
-          (mount-point "/")
-          (device "/dev/mapper/cryptroot")
-          (type "ext4")
-          (dependencies mapped-devices))
-         %base-file-systems)))
+   (bootloader
+    (bootloader-configuration
+     (bootloader grub-efi-bootloader)
+     (targets (list "/boot/efi"))
+     (keyboard-layout keyboard-layout)))
+   (mapped-devices
+    (list (mapped-device
+           (source
+            (uuid "34627d93-4e91-42e8-8eb8-b64c8fa4729f"))
+           (target "cryptroot")
+           (type luks-device-mapping))))
+   (file-systems
+    (cons* (file-system
+            (mount-point "/boot/efi")
+            (device (uuid "C899-DB8A" 'fat32))
+            (type "vfat"))
+           (file-system
+            (mount-point "/")
+            (device "/dev/mapper/cryptroot")
+            (type "ext4")
+            (dependencies mapped-devices))
+           %base-file-systems))))
