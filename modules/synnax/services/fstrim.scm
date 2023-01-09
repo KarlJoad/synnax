e5bff0a9e79d4675c356fcc3bb913d0e53fdcaa4
Karl Hallsby <karl@hallsby.com>
2023-01-08 18:47:52 -0600

    services: fstrim: Add service type

---
+++ modules/synnax/services/fstrim.scm
@@ -0,0 +1,39 @@
+(define-module (synnax services fstrim)
+  #:use-module (gnu services)
+  #:use-module (gnu services shepherd)
+  #:use-module (gnu services mcron)
+  #:use-module (guix gexp)
+  #:use-module (guix records)
+  #:use-module (gnu packages linux)
+  #:use-module (srfi srfi-1)
+  #:export (fstrim-service-type
+            fstrim-configuration fstrim-configuration?))
+
+(define-record-type* <fstrim-configuration>
+  fstrim-configuration make-fstrim-configuration
+  fstrim-configuration?
+  (interval fstrim-configuration-interval ;integer (seconds)
+            (default (* 60 60 24 7))))
+
+(define (fstrim-job configuration)
+  ;; The time to run the job must be set by calculating the offset from now to
+  ;; 7 days from now using math. This is needed because despite having
+  ;; documentation that next-week and next-week-from exists, there are no procedures
+  ;; defined in mcron for them.
+  ;; Patch https://lists.gnu.org/archive/html/bug-mcron/2022-09/msg00000.html would
+  ;; add this functionality, but remains to be merged into upstream mcron.
+  (list
+   #~(job (lambda (current-time)
+            (+ current-time #$(fstrim-configuration-interval configuration)))
+          #$(file-append util-linux "/bin/fstrim --listed-in /etc/fstab:/proc/self/mountinfo --verbose --quiet-unsupported"))))
+
+(define fstrim-service-type
+  (service-type
+   (name 'fstrim)
+   (description
+    "Periodically run TRIM command on all SSDs using fstrim.
+By default, fstrim runs weekly.")
+   (extensions (list (service-extension mcron-service-type
+                                        fstrim-job)))
+   (compose concatenate)
+   (default-value (fstrim-configuration))))
