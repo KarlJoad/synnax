c786703d963f60119ab097882c9ea14ace10bb11
Karl Hallsby <karl@hallsby.com>
2023-03-28 19:00:18 -0500

    packages: add script to grab last system/home reconfigure date
    This script is currently slightly limited by the way it is defined. It
    currently only fetches the last reconfiguration date of the karljoad
    user, which may not exist. This needs to be changed to a command-line
    parameter in the future.

--- modules/synnax/packages/scripts.scm
+++ modules/synnax/packages/scripts.scm
@@ -42,3 +42,49 @@ my monitors to work correctly.")
                            (format #t "~a~%" (string-append cmd-to-run))
                            (system cmd-to-run)))
                      (xrandr-cmd "HDMI-1" "--left-of DVI-D-1")))))
+
+(define-public last-reconfigure-date
+  (package
+   (name "last-reconfigure-date")
+   (version "git")
+   (source #f)
+   (native-inputs `(("last-reconfigure-date-script" ,last-reconfigure-date-script)))
+   (build-system trivial-build-system)
+   (arguments
+    `(#:modules ((guix build utils) (srfi srfi-13))
+      #:builder
+      (begin
+        (use-modules (guix build utils)
+                     (srfi srfi-13))
+        (let* ((bin (string-append (assoc-ref %outputs "out") "/bin"))
+               (script-source (assoc-ref %build-inputs "last-reconfigure-date-script"))
+               (target-script-file (string-append bin "/" "last-reconfigure-date.scm")))
+          (mkdir-p bin)
+          (symlink script-source target-script-file)))))
+   (home-page "https://github.com/KarlJoad/synnax")
+   (synopsis "Script that displays when the system and home were last reconfigured")
+   (description "Read from /var/guix/profiles/ to determine the last time the
+most recent system and per-user home profile were built.")
+   (license #f)))
+
+;; TODO: Make more generic.
+;; Should accept a username parameter for /var/guix/profiles/per-user
+;; Should accept an enum between home or system
+;; home = /var/guix/profiles/per-user/<username>/guix-home
+;; system = /var/guix/profiles/system
+(define last-reconfigure-date-script
+  (program-file "last-reconfigure-date"
+                (with-imported-modules
+                 `((srfi srfi-19))
+                 #~(begin
+                     (use-modules (srfi srfi-19))
+                     (define (file-ctime file-path)
+                       (let* ((file-info (stat file-path))
+                              (file-ctime (stat:ctime file-info))
+                              (epoch-secs (make-time time-monotonic 0 file-ctime))
+                              (file-cdate (time-monotonic->date epoch-secs)))
+                         (format #f "~a"
+                                 (date->string file-cdate "~c"))))
+                     (format #t "Last reconfigure dates:~%")
+                     (format #t "System:\t~a~%" (file-ctime "/var/guix/profiles/system"))
+                     (format #t "Home:\t~a~%" (file-ctime "/var/guix/profiles/per-user/karljoad/guix-home"))))))
