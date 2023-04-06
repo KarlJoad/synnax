0264c316cf286647c0f9f1ef41fb00e7d51f618f
Karl Hallsby <karl@hallsby.com>
2023-04-06 12:52:43 -0500

    systems: home: add automatic direnv shell support to bash
    direnv changes environment variables for youb ased on the project and
    a .envrc file in the base of the project tree. Adding this eval hook
    to the bottom of the .bashrc file means Bash will automatically pick
    up direnv settings on-the-fly.
    
    https://direnv.net/docs/hook.html
    
    Direnv comes last so that the generated configuration ends up at the
    end of the generated .bashrc file. That way direnv is the last thing
    to be set up when starting a new shell.

--- modules/synnax/systems/home.scm
+++ modules/synnax/systems/home.scm
@@ -96,7 +96,9 @@
                 ("ll" . "'ls -la'")
                 ("ls='ls -p --color'" . "auto")))
              (bashrc
-              (list (local-file "bashrc" "bashrc")))
+              (list (local-file "bashrc" "bashrc")
+                    ;; NOTE: Adding direnv support should come last!
+                    (plain-file "bashrc-add-direnv" "eval \"$(direnv hook bash)\"")))
              (bash-profile
               (list (local-file "bash_profile" "bash_profile")))))
    (service home-openssh-service-type
