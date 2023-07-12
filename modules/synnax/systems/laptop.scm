be2262f75a0fab3d939bab8b676f294c440ddaf0
Karl Hallsby <karl@hallsby.com>
2023-07-12 12:44:14 -0500

    systems: Move laptop to shared/inherited config

--- modules/synnax/systems/laptop.scm
+++ modules/synnax/systems/laptop.scm
@@ -1,71 +1,31 @@
-(define-module (synnax systems laptop))
+(define-module (synnax systems laptop)
+  #:export (laptop))
 
 (use-modules (gnu)
-             (gnu packages linux)
-             (guix download)
-             (guix packages)
-             (nongnu packages linux)
-             (nongnu system linux-initrd)
-             (synnax systems packages))
+             (synnax systems base-system))
+
+(use-package-modules
+ linux ;; brightnessctl
+ )
 
 (use-service-modules
- cups
- desktop
- networking
- ssh
- syncthing
- xorg
- nix
- virtualization docker)
+ desktop)
 
 (define-public laptop
   (operating-system
-   (locale "en_US.utf8")
-   (kernel linux)
-   (initrd microcode-initrd)
-   (firmware (list linux-firmware))
-
-   (timezone "America/Chicago")
-   (keyboard-layout (keyboard-layout "us"))
+   (inherit %base-system)
    (host-name "Guix-Laptop")
-   (users (cons* (user-account
-                  (name "karljoad")
-                  (comment "Karl Hallsby")
-                  (group "users")
-                  (home-directory "/home/karljoad")
-                  (supplementary-groups
-                   `("wheel" "netdev" "audio" "video"
-                     "lp" "input" "tty"
-                     "kvm" ;; ,(user-group (name "libvirt-users") (system? #t))
-                     "dialout")))
-                 %base-user-accounts))
+
    (packages
     (append
      (list brightnessctl
            bluez)
-     %system-packages
-     %base-packages))
+     (operating-system-packages %base-system)))
 
    (services
     (append
-     (list (service xfce-desktop-service-type)
-           (service openssh-service-type)
-           (service cups-service-type)
-           (set-xorg-configuration
-            (xorg-configuration
-             (keyboard-layout keyboard-layout)))
-           (service nix-service-type)
-           (service syncthing-service-type
-                    (syncthing-configuration
-                     (user "karljoad") ;; TODO: Refactor `user' field to use variable.
-                     (arguments '("-gui-address=127.0.0.1:8384"))))
-           (service libvirt-service-type
-                    (libvirt-configuration
-                     (unix-sock-group "libvirt-users")))
-           (service virtlog-service-type)
-           (service docker-service-type)
-           (service bluetooth-service-type))
-     %desktop-services))
+     (list (service bluetooth-service-type))
+     (operating-system-user-services %base-system)))
 
    (bootloader
     (bootloader-configuration
@@ -80,12 +40,14 @@
            (type luks-device-mapping))))
    (file-systems
     (cons* (file-system
-            (mount-point "/boot/efi")
-            (device (uuid "C899-DB8A" 'fat32))
-            (type "vfat"))
+             (mount-point "/boot/efi")
+             (device (uuid "C899-DB8A" 'fat32))
+             (type "vfat"))
            (file-system
-            (mount-point "/")
-            (device "/dev/mapper/cryptroot")
-            (type "ext4")
-            (dependencies mapped-devices))
+             (mount-point "/")
+             (device "/dev/mapper/cryptroot")
+             (type "ext4")
+             (dependencies mapped-devices))
            %base-file-systems))))
+
+laptop
