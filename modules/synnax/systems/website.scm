c6b5720efd3dd91cc5559102d803f1a39a698192
Karl Hallsby <karl@hallsby.com>
2023-08-10 21:38:41 -0500

    systems: website: Add cgit for browsing my repos with a browser

--- modules/synnax/systems/website.scm
+++ modules/synnax/systems/website.scm
@@ -71,6 +71,14 @@
                         (root personal-website)
                         (locations
                          (list
+                          ;; Redirect /cgit traffic to cgit server
+                          (nginx-location-configuration
+                           (uri "/cgit")
+                           (body '("return 308 $scheme://cgit.karl.hallsby.com ;")))
+                          ;; /cgit/repo.git redirects to cgit.karl.hallsby.com/repo.git
+                          (nginx-location-configuration
+                           (uri "~ /cgit(/.*)")
+                           (body '("return 308 $scheme://cgit.karl.hallsby.com$1 ;")))
                           ;; NOTE: git-http is for cloning using HTTP, not browsing!
                           ;; If you browse, you will always get a black webpage
                           (git-http-nginx-location-configuration
@@ -87,6 +95,41 @@
            ;;             (certificate-configuration
            ;;              (domains '("karl.hallsby.com" "www.karl.hallsby.com"))
            ;;              (deploy-hook %certbot-nginx-deploy-hook))))))
+           (service cgit-service-type
+                    (cgit-configuration
+                     (root-desc "Code is made for sharing!") ;; Thanks samplet!
+                     (repository-directory "/srv/git/")
+                     (enable-git-config? #t) ;; cgit reads repo info from .git/config
+                     (virtual-root "/") ;; Build links relative to this
+                     (nginx
+                      (list
+                       (nginx-server-configuration
+                        (listen '("80"))
+                        (server-name '("cgit.karl.hallsby.com"))
+                        (root "/srv/git/") ;; Sets $document_root
+                        (index '("index.html"))
+                        (try-files (list "$uri" "@cgit"))
+                        (server-tokens? #f)
+                        (locations
+                         (list
+                          (nginx-location-configuration ;; So CSS & co. are found
+                           (uri "~ ^/share/cgit/")
+                           (body `(("root " ,cgit ";"))))
+                          (nginx-location-configuration
+                           (uri "~ /cgit(/.*)") ;; server/git/repo.git is for HTTP git cloning
+                           (body `(("fastcgi_param SCRIPT_FILENAME " ,cgit "/lib/cgit/cgit.cgi;")
+                                   ;; () is Nginx regex captures, numbered from 1
+                                   "fastcgi_param PATH_INFO $1;" ; Grab /repo.git and use that as the path
+                                   "fastcgi_param QUERY_STRING $args;"
+                                   "fastcgi_param HTTP_HOST $server_name;"
+                                   "fastcgi_pass 127.0.0.1:9000;")))
+                          (nginx-named-location-configuration
+                           (name "cgit")
+                           (body `(("fastcgi_param SCRIPT_FILENAME " ,cgit "/lib/cgit/cgit.cgi;")
+                                   "fastcgi_param PATH_INFO $uri;"
+                                   "fastcgi_param QUERY_STRING $args;"
+                                   "fastcgi_param HTTP_HOST $server_name;"
+                                   "fastcgi_pass 127.0.0.1:9000;"))))))))))
            (service fail2ban-service-type
                     (fail2ban-configuration
                      (extra-jails
