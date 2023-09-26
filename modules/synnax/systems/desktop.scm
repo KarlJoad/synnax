2f13711f4da533c762699907971adc9c12a1845f
Karl Hallsby <karl@hallsby.com>
2023-09-25 23:32:59 -0500

    systems: desktop: Remove attempt at automount NAS as CIFS store

--- modules/synnax/systems/desktop.scm
+++ modules/synnax/systems/desktop.scm
@@ -125,14 +125,6 @@ is turned into
                (uuid "892ca4b1-5ffa-45ef-ba27-c6e6a55097c5"
                      'ext4))
               (type "ext4"))
-            (file-system
-              (type "cifs")
-              (mount-point "/mnt/store")
-              (device "//karl-nas.raven/store")
-              (options "noauto,vers=default,iocharset=utf8,uid=1000,gid=998,rw,file_mode=0644,dir_mode=0755,nobrl,nounix,credentials=/root/.personalSMBcredentials")
-              (mount? #f)
-              (create-mount-point? #t)
-              (mount-may-fail? #t))
             %base-file-systems))))
 
 desktop
