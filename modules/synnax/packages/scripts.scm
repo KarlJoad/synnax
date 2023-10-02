8b342c6098d9230e1481f658092bd59ab4b24805
Karl Hallsby <karl@hallsby.com>
2023-10-01 22:11:22 -0500

    packages: script: last-reconfigure-date script uses running user's name

--- modules/synnax/packages/scripts.scm
+++ modules/synnax/packages/scripts.scm
@@ -116,16 +116,19 @@ most recent system and per-user home profile were built.")
 (define last-reconfigure-date-script
   (program-file "last-reconfigure-date"
                 (with-imported-modules
-                 `((srfi srfi-19))
-                 #~(begin
-                     (use-modules (srfi srfi-19))
-                     (define (file-ctime file-path)
-                       (let* ((file-info (stat file-path))
-                              (file-ctime (stat:ctime file-info))
-                              (epoch-secs (make-time time-monotonic 0 file-ctime))
-                              (file-cdate (time-monotonic->date epoch-secs)))
-                         (format #f "~a"
-                                 (date->string file-cdate "~c"))))
-                     (format #t "Last reconfigure dates:~%")
-                     (format #t "System:\t~a~%" (file-ctime "/var/guix/profiles/system"))
-                     (format #t "Home:\t~a~%" (file-ctime "/var/guix/profiles/per-user/karljoad/guix-home"))))))
+                    `((srfi srfi-19))
+                  #~(begin
+                      (use-modules (srfi srfi-19))
+                      (define (file-ctime file-path)
+                        (let* ((file-info (stat file-path))
+                               (file-ctime (stat:ctime file-info))
+                               (epoch-secs (make-time time-monotonic 0 file-ctime))
+                               (file-cdate (time-monotonic->date epoch-secs)))
+                          (format #f "~a"
+                                  (date->string file-cdate "~c"))))
+                      (let ((user-name-running (passwd:name (getpwnam (getuid)))))
+                        (format #t "Last reconfigure dates:~%")
+                        (format #t "System:\t~a~%" (file-ctime "/var/guix/profiles/system"))
+                        (format #t "Home:\t~a~%"
+                                (file-ctime
+                                 (format #f "/var/guix/profiles/per-user/~a/guix-home" user-name-running))))))))
