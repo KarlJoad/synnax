dca8c951645aff3ff2b4875455bdbf2fe8cb95bf
Karl Hallsby <karl@hallsby.com>
2023-10-29 19:16:20 -0500

    packages: guile: guile-basexx use tagged version

--- modules/synnax/packages/guile-xyz.scm
+++ modules/synnax/packages/guile-xyz.scm
@@ -15,29 +15,27 @@
   #:use-module (gnu packages gnupg))
 
 (define-public guile-basexx
-  (let ((commit "8026bd0373c3b9eaed0c23b1c10f267471cafea5")
-        (revision "0"))
   (package
-    (name "guile-basexx")
-    (version (git-version "0.0.1" revision commit))
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://github.com/KarlJoad/guile-basexx.git")
-                    (commit commit)))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32
-                "1kmn42f40ilz6pirqxhcdxfha7vfyp29lwf79cqq1xdl68lgwdap"))))
-    (build-system gnu-build-system)
-    (native-inputs
-     (list autoconf automake pkg-config texinfo guile-3.0))
-    (inputs
-     (list guile-gcrypt))
-    (home-page "https://github.com/KarlJoad/guile-basexx.git")
-    (synopsis "Base16/32/64 encoding/decoding library for Guile")
-    (description "Base16/32/64 encoding/decoding library for Guile.
+   (name "guile-basexx")
+   (version "0.0.1")
+   (home-page "https://github.com/KarlJoad/guile-basexx")
+   (source (origin
+            (method git-fetch)
+            (uri (git-reference
+                  (url (string-append home-page ".git"))
+                  (commit (string-append "v" version))))
+            (file-name (git-file-name name version))
+            (sha256
+             (base32
+              "0akgi3vfnh9vjr3icchzxd7ps9l5wn1crdd61xl7jxsiq980rjhg"))))
+   (build-system gnu-build-system)
+   (native-inputs
+    (list autoconf automake pkg-config texinfo guile-3.0))
+   (inputs
+    (list guile-gcrypt))
+   (synopsis "Base16/32/64 encoding/decoding library for Guile")
+   (description "Base16/32/64 encoding/decoding library for Guile.
 This is a @b{copy} of the base16/32/64 implementation in Guix! It has been
 factored out to a separate library for any other project that wants to be able
 to handle various base-encodings.")
-    (license license:gpl3+))))
+   (license license:gpl3+)))
