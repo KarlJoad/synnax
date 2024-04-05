d044fa557398eb0e79ab68272850ebff7e4a9fa1
Karl Hallsby <karl@hallsby.com>
2024-04-04 22:54:21 -0500

    packages: Init tree-sitter-latex at 0.3.0

--- modules/synnax/packages/tree-sitter.scm
+++ modules/synnax/packages/tree-sitter.scm
@@ -11,3 +11,10 @@
    "1.0.8"
    ;; Version 1.2.1 is most recent, but requires tree-sitter >0.21.0
    #:repository-url "https://github.com/tlaplus-community/tree-sitter-tlaplus"))
+
+(define-public tree-sitter-latex
+  ((@@ (gnu packages tree-sitter) tree-sitter-grammar)
+   "latex" "LaTeX"
+   "0lc42x604f04x3kkp88vyqa5dx90wqyisiwl7nn861lyxl6phjnf"
+   "0.3.0"
+   #:repository-url "https://github.com/latex-lsp/tree-sitter-latex"))
