a775c6ce2a0b1b33c240ca4306f1c30622e1e632
Karl Hallsby <karl@hallsby.com>
2024-06-10 14:53:08 -0500

    systems: home: Update NixOS channel to 24.05

--- modules/synnax/systems/home.scm
+++ modules/synnax/systems/home.scm
@@ -258,7 +258,7 @@ setw -g aggressive-resize on"))))
                    home-files-service-type
                    (list `(".nix-channels"
                            ,(plain-file "nix-channels"
-                                        "https://nixos.org/channels/nixos-23.11 nixpkgs"))
+                                        "https://nixos.org/channels/nixos-24.05 nixpkgs"))
                          `(".config/nixpkgs/config.nix"
                            ,(plain-file "config.nix"
                                         "{
