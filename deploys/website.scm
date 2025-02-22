a2e062c8c124313c11287e95f89a55c8f52cd1e0
Karl Hallsby <karl@hallsby.com>
2025-02-22 17:32:39 -0600

    deploys: website: Use website DNS name instead of IP address for deploys
    This is nothing other than a QoL improvement for me, since (in theory)
    the IP address of the VM I use for Guix _could_ change. But that has
    never happened, so this is more just to line up with my philosophy
    that configurations should be as IP address agnostic as possible.

--- deploys/website.scm
+++ deploys/website.scm
@@ -6,7 +6,7 @@
        (environment managed-host-environment-type)
        (configuration (machine-ssh-configuration
                        ;; IP or DNS-resolved address of machine(s) to manage
-                       (host-name "107.191.51.74")
+                       (host-name "karl.hallsby.com")
                        (system "x86_64-linux")
                        ;; Public SSH host key of system being configured
                        (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFPpUuW06rpKos6uboMvqKqh5h0vU7o6awZrRtNYtTIb root@(none)")
