8db8844c3a9311173c640f335ac334096006cd0a
Karl Hallsby <karl@hallsby.com>
2025-07-08 22:40:43 -0500

    packages: Init Beam Book at 1.0

--- modules/synnax/packages/books.scm
+++ modules/synnax/packages/books.scm
@@ -2,8 +2,20 @@
   #:use-module (guix gexp)
   #:use-module (guix packages)
   #:use-module (guix download)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system gnu)
   #:use-module (guix build-system ruby)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages curl)
+  #:use-module (gnu packages erlang)
+  #:use-module (gnu packages graphviz)
+  #:use-module (gnu packages java)
+  #:use-module (gnu packages java-graphics)
+  #:use-module (gnu packages rsync)
+  #:use-module (gnu packages ruby)
   #:use-module (gnu packages ruby-xyz)
+  #:use-module (gnu packages version-control)
+  #:use-module (gnu packages wget)
   #:use-module ((guix licenses) #:prefix license:))
 
 ;; TODO: Put this in Guix!
@@ -52,3 +64,73 @@
     (description "Ditaa JAR files wrapped in a Ruby gem.")
     (home-page "https://github.com/asciidoctor/asciidoctor-diagram")
     (license license:expat)))
+
+(define-public beambook
+  (package
+    (name "beam-book")
+    (version "1.0")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/happi/theBeamBook")
+                    (commit (string-append "v" version))
+                    (recursive? #t)))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "0gzmdlp6h8lwfanahppx8r475chk8k460g7qpavccvp7bagblh86"))))
+    (build-system gnu-build-system)
+    (native-inputs
+     (list git-minimal ; Only used to build author/contributor list
+           rsync
+           erlang
+           (origin
+             (method git-fetch)
+             (uri (git-reference
+                   (url "https://github.com/erlang/otp")
+                   ;; The exact commit used is important ONLY IF generic
+                   ;; instructions emitted by the compiler change.
+                   (commit "OTP-28.0")))
+             (file-name (git-file-name name "OTP-28.0"))
+             (sha256
+              (base32 "18glv9wh05i4jvr1snq92m7rr1wkiixw9kx74h0ywcgx1p021rps")))
+           gnu-make
+           ruby
+           openjdk
+           graphviz
+           ditaa
+           ruby-asciidoctor
+           ruby-asciidoctor-pdf
+           ruby-asciidoctor-diagram
+           ruby-asciidoctor-diagram-ditaamini
+           ruby-rouge))
+    (arguments
+     (list
+      ;; #:make-flags #~(list "pdf-a4" "pdf-publish" "html" "epub")
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-after 'unpack 'copy-beam-genop.tab
+            (lambda* (#:key inputs #:allow-other-keys)
+              (let ((otp (assoc-ref inputs "beam-book-OTP-28.0-checkout")))
+                (copy-recursively
+                 (string-append otp "/lib/compiler/src/genop.tab")
+                 "genop.tab"))))
+          (add-before 'build 'set-font-env-vars
+            (lambda* _
+              (setenv "XDG_CACHE_HOME" "/tmp")))
+          (delete 'configure)
+          ;; TODO: Build epub and pdf-publish versions of book too.
+          (replace 'install
+            (lambda* (#:key outputs #:allow-other-keys)
+              (let ((out (assoc-ref outputs "out")))
+                (mkdir-p out)
+                (install-file "beam-book-a4.pdf" out)
+                (copy-recursively "site" out))))
+          (delete 'check))))
+    (home-page "https://github.com/happi/theBeamBook/")
+    (synopsis "")
+    (description "")
+    (license
+     (list license:cc-by-sa4.0 ;; The book itself
+           ;; Erlang's genop.tab
+           license:asl2.0))))
