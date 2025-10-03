9aa15d6412c131c90f3372c59a43016a2672f01b
Karl Hallsby <karl@hallsby.com>
2025-10-03 10:16:30 -0500

    shells: program-performance: Change use-module to reflect upstream
    Upstream Guix moved kcachegrind from (gnu packages kde) to
    (gnu packages kde-sdk) in commit 4d0d955555f1651eb7210a6d60fc884a91d18afe.

--- modules/synnax/shells/program-performance.scm
+++ modules/synnax/shells/program-performance.scm
@@ -1,7 +1,7 @@
 (define-module (synnax shells program-performance)
   #:use-module (guix profiles)
   #:use-module (gnu packages valgrind)
-  #:use-module (gnu packages kde))
+  #:use-module (gnu packages kde-sdk))
 
 (packages->manifest
  (list valgrind
